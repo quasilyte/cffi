cbf3a187ab90845063e1bbd1c66be5df3502a5f8
quasilyte <quasilyte@gmail.com>
2017-08-18 01:03:36 +0300

    initial commit

---
+++ funcall/funcall.go
@@ -0,0 +1,75 @@
+package funcall
+
+import (
+	"reflect"
+)
+
+type (
+	// User-defined function that converts Go value to C value.
+	go2CFunc func(interface{}) interface{}
+	// User-defined function that converts C value to Go value.
+	c2GoFunc func(interface{}) interface{}
+)
+
+// Invoker exposes methods that can invoke C functions with Go types.
+type Invoker struct {
+	go2c go2CFunc
+	c2go c2GoFunc
+}
+
+// NewInvoker creates C functions invoker that can handle some
+// Go->C and C->Go types convertions.
+// The set of supported conversions depends on the provided
+// callbacks.
+func NewInvoker(go2c go2CFunc, c2go c2GoFunc) *Invoker {
+	return &Invoker{
+		go2c: go2c,
+		c2go: c2go,
+	}
+}
+
+// Apply is like "Call", but accepts a slice of arguments instead of being
+// variadic over interface{}.
+func (inv *Invoker) Apply(callable interface{}, args []interface{}) interface{} {
+	xs := inv.mapArgs(args)
+	fn := reflect.ValueOf(callable)
+	return inv.c2go(fn.Call(xs)[0].Interface())
+}
+
+// Call invokes "callable" argument with provided "args".
+// Each arg is expected to be a Go value which can be converted
+// to C value with Invoker go2c callback.
+// Return value should be convertible to Go type with Invoker c2go callback.
+func (inv *Invoker) Call(callable interface{}, args ...interface{}) interface{} {
+	return inv.Apply(callable, args)
+}
+
+// Int is a convenience wrapper around "Call" which does type assertion for you.
+func (inv *Invoker) Int(callable interface{}, args ...interface{}) int {
+	return inv.Apply(callable, args).(int)
+}
+
+// String is a convenience wrapper around "Call" which does type assertion for you.
+func (inv *Invoker) String(callable interface{}, args ...interface{}) string {
+	return inv.Apply(callable, args).(string)
+}
+
+// Convert Go values slice to C values slice.
+func (inv *Invoker) mapArgs(args []interface{}) []reflect.Value {
+	xs := make([]reflect.Value, 0, len(args))
+	for i := range args {
+		x := inv.go2c(args[i])
+		// To support 1->N argument mapping,
+		// handle slices as a tuple-like argument
+		// that should be unwrapped into len(x) C args.
+		// No need for recursive translation here.
+		if y, ok := x.([]interface{}); ok {
+			for j := range y {
+				xs = append(xs, reflect.ValueOf(y[j]))
+			}
+		} else {
+			xs = append(xs, reflect.ValueOf(x))
+		}
+	}
+	return xs
+}
